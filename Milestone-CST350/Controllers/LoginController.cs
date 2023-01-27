bfa0e52ad03dd6aeac144a137f15812a3edf7e70
Jugooch <justicegooch@hotmail.com>
2023-01-27 08:39:49 -0700

    Adding a Login Module

---
+++ Milestone-CST350/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Milestone_CST350.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
